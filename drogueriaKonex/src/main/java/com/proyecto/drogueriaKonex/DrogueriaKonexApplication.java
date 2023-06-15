f6295483c716feda72f357b28770de419ff8071a
gigomez <gloirgopa@hotmail.com>
2023-06-14 21:22:58 -0500

    proyecto base

---
+++ drogueriaKonex/src/main/java/com/proyecto/drogueriaKonex/DrogueriaKonexApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.drogueriaKonex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrogueriaKonexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrogueriaKonexApplication.class, args);
+	}
+
+}
---
+++ drogueriaKonex/src/test/java/com/proyecto/drogueriaKonex/DrogueriaKonexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.drogueriaKonex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogueriaKonexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
